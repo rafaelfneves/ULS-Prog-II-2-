8f2e343bd9bf896eaa6d9e70a982c343e951872a
Rafael Neves <43051996+rafaelfneves@users.noreply.github.com>
2019-08-28 14:32:54 -0300

    Add files via upload

---
+++ SKETCH.c
@@ -0,0 +1,14 @@
+#include <locale.h>
+#include <string.h>
+#include <stdio.h>
+#include <conio.h>
+
+int main(){
+	setlocale(LC_ALL,"Portuguese");
+
+	printf("\n\n\n");
+	printf("\n|===============================================================================|");
+	printf("\n|    --[© 2019 Rafael Neves All Rights Reserved (github.com/rafaelfneves)]--    |");
+	printf("\n|===============================================================================|\n");
+	return 0;	
+}
