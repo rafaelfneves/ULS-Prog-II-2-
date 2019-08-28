4ad352df508297490154d5cc767d8ebc6121366b
Rafael Neves <43051996+rafaelfneves@users.noreply.github.com>
2019-08-28 14:55:30 -0300

    Add files via upload

--- Lista_01/Q4.c
+++ Lista_01/Q4.c
@@ -8,8 +8,8 @@ um terceiro VETOR C com os elementos diferentes existentes nos vetores A e B.*/
 int main(){
 	setlocale(LC_ALL,"Portuguese");
 	
-	int vetA[40],vetB[50],vetC[10];	
-	int i;
+	int vetA[40],vetB[50],vetC[10],cont;	
+	int i,j;
 	
 	//Gerar número aleatório de A
 	for(i=0;i<40;i++){
@@ -22,11 +22,32 @@ int main(){
 		fflush(stdin);
 	}
 	
-	//Gerar número aleatório de C
-	for(i=0;i<10;i++){
-		
+	//Gerar VETOR de C
+	for(i=0;i<50;i++){
+		for(j=0;j<40;j++){
+			//Randomizar cont
+			cont=rand() % 100;
+			if(cont != vetA[i] && cont != vetB[i])
+			vetC[i]=cont;
+		}	
 	}
-
+	
+	//Print A
+	printf("|===A===|\n");
+	for(i=0;i<40;i++){
+		printf("|%d",vetA[i]);
+	}
+	//Print B
+	printf("\n\n|===B===|\n");
+	for(i=0;i<50;i++){
+		printf("|%d",vetB[i]);
+	}
+	//Print C
+	printf("\n\n|===C===|\n");
+	for(i=0;i<10;i++){
+		printf("|%d",vetC[i]);
+	}
+	
 	printf("\n\n\n");
 	printf("\n|===============================================================================|");
 	printf("\n|    --[© 2019 Rafael Neves All Rights Reserved (github.com/rafaelfneves)]--    |");
